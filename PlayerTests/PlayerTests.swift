9ae1449afa1c11d6ab1281f8e8de9bc1f2c705fd
importants <dlwognsdk@tukorea.ac.kr>
2024-10-17 19:19:52 +0900

    build: 프로젝트 init

---
+++ PlayerTests/PlayerTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlayerTests.swift
+//  PlayerTests
+//
+//  Created by 이재훈 on 10/17/24.
+//
+
+import Testing
+@testable import Player
+
+struct PlayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ PlayerUITests/PlayerUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  PlayerUITestsLaunchTests.swift
+//  PlayerUITests
+//
+//  Created by 이재훈 on 10/17/24.
+//
+
+import XCTest
+
+final class PlayerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
