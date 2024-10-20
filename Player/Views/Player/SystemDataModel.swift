a1898b04de32fbb6ab7652ba8e0373edf6254716
Jae Hun Lee <118325810+importantsgit@users.noreply.github.com>
2024-10-20 10:11:02 +0900

    feat: 시스템 변수에 접근할 수 있는 DataModel 생성
    추가 사항
    - 볼륨 / 밝기를 control할 수 있는 DataModel 추가

---
+++ Player/Views/Player/SystemDataModel.swift
@@ -0,0 +1,86 @@
+//
+//  File.swift
+//  Player
+//
+//  Created by Importants on 10/20/24.
+//
+
+import Combine
+import MediaPlayer
+
+final class SystemDataModel: ObservableObject {
+    struct BrightnessValue {
+        var origin: CGFloat
+        var changed: CGFloat
+    }
+    
+    struct VolumeValue {
+        var origin: Float
+        var changed: Float
+    }
+    
+    @Published var brightnessValue: BrightnessValue = .init(
+        origin: UIApplication.currentBrightness,
+        changed: UIApplication.currentBrightness
+    )
+    
+    @Published var volumeValue: VolumeValue = .init(
+        origin: AVAudioSession.currentVolume,
+        changed: AVAudioSession.currentVolume
+    )
+    
+    var combinedPublisher: AnyPublisher<(BrightnessValue, VolumeValue), Never> {
+        Publishers.CombineLatest($brightnessValue, $volumeValue)
+            .eraseToAnyPublisher()
+    }
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    func setBrightness(value: CGFloat) {
+        let currentValue = reduceValue(value: value)
+        brightnessValue.changed = currentValue
+        UIApplication.setBrightness(currentValue)
+    }
+    
+    func setVolume(value: Float) {
+        let currentValue = reduceValue(value: value)
+        volumeValue.changed = currentValue
+        MPVolumeView.view.setVolume(
+            volume: currentValue
+        )
+    }
+}
+
+private extension SystemDataModel {
+    func reduceValue<T: Comparable & FloatingPoint>(value: T) -> T {
+        min(max(value, 0), 1)
+    }
+}
+
+extension UIApplication {
+    // 값 반환
+    static var currentBrightness: CGFloat {
+        UIApplication.currentWindow?.windowScene?.screen.brightness ?? 0.5
+    }
+    
+    // 값 설정
+    static func setBrightness(_ value: CGFloat) {
+        UIApplication.currentWindow?.windowScene?.screen.brightness = value
+    }
+}
+
+extension MPVolumeView {
+    static let view = MPVolumeView()
+    
+    func setVolume(volume: Float) {
+        guard let slider = MPVolumeView.view.subviews.first(where: { $0 is UISlider }) as? UISlider
+        else { return }
+        slider.value = volume
+    }
+}
+
+extension AVAudioSession {
+    static var currentVolume: Float {
+        AVAudioSession.sharedInstance().outputVolume
+    }
+}
